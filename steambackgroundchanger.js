18bb6cfb482e066f0bfe15ae8c13906110651329
yea <15997566+yea@users.noreply.github.com>
2023-12-29 06:11:46 -0600

    made more gooder

--- steambackgroundchanger.js
+++ steambackgroundchanger.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         steambackgroundchanger
 // @namespace    https://github.com/yea/steam-background-changer
-// @version      1.0
+// @version      1.1
 // @description  easily preview steam backgrounds on any profile, including yours or anyone else's, before purchasing
 // @author       neversince
 // @match        https://steamcommunity.com/id/*
@@ -30,14 +30,104 @@
 
         existingElement.appendChild(inputElement);
 
-        function handleEnterKeyPress() {
-            var inputValue = inputElement.value;
-            console.log('Submitted:', inputValue);
+        function hasFileExtension(filename, extension) {
+            return filename.endsWith(`.${extension}`);
+        }
 
-            var elementsWithClass = document.getElementsByClassName('no_header profile_page has_profile_background full_width_background');
+        function doesClassExist(className) {
+            var elementsWithClass = document.getElementsByClassName(className);
+            return elementsWithClass.length > 0;
+        }
+
+        function getCurrentBackgroundType() {
+            if (doesClassExist('profile_animated_background')) {
+                return 'animated';
+            } else if (doesClassExist('no_header profile_page ')) {
+                var elementsWithClass = document.getElementsByClassName('no_header profile_page ');
+
+                if (elementsWithClass[0].style.cssText.trim() !== '') {
+                    return 'static';
+                } else {
+                    return 'none';
+                }
+            } else {
+                return 'none';
+            }
+            
+        }
+
+        function handleStaticBackround(inputValue, currentBackgroundType) {
+            if (currentBackgroundType == 'animated') {
+                var elementsToRemove = document.getElementsByClassName('profile_animated_background');
+                elementsToRemove[0].remove();
+            }
+
+            var elementsWithClass = document.getElementsByClassName('no_header profile_page ');
 
             if (elementsWithClass.length > 0) {
-                elementsWithClass[0].style.backgroundImage = `url('${inputValue}')`;
+                elementsWithClass[0].classList.add('has_profile_background');
+                elementsWithClass[0].classList.add('full_width_background');
+                elementsWithClass[0].style.backgroundImage = `url( '${inputValue}' )`;
+            }
+        }
+
+        function handleAnimatedBackround(inputValue, currentBackgroundType) {
+            if (currentBackgroundType == 'static' || currentBackgroundType == 'none') {
+                var targetElement = document.querySelector('.no_header.profile_page');
+
+                if (targetElement) {
+                    targetElement.classList.add('has_profile_background');
+                    targetElement.classList.add('full_width_background');
+
+                    var animatedBackgroundDiv = document.createElement('div');
+                    animatedBackgroundDiv.classList.add('profile_animated_background');
+
+                    var videoElement = document.createElement('video');
+                    videoElement.setAttribute('playsinline', '');
+                    videoElement.setAttribute('autoplay', '');
+                    videoElement.setAttribute('muted', '');
+                    videoElement.setAttribute('loop', '');
+
+                    var sourceMp4 = document.createElement('source');
+                    sourceMp4.setAttribute('src', inputValue);
+                    sourceMp4.setAttribute('type', 'video/mp4');
+
+                    videoElement.appendChild(sourceMp4);
+
+                    animatedBackgroundDiv.appendChild(videoElement);
+
+                    targetElement.appendChild(animatedBackgroundDiv);
+
+                    targetElement.insertBefore(animatedBackgroundDiv, targetElement.firstChild);
+                }
+            } else {
+                var elementsWithClass = document.getElementsByClassName('profile_animated_background');
+
+                if (elementsWithClass.length > 0) {
+                    var videos = document.querySelectorAll('video');
+                    videos.forEach(function(video) {
+                        var sources = video.getElementsByTagName('source');
+                        for (var i = 0; i < sources.length; i++) {
+                            if (sources[i].type === 'video/mp4') {
+                                sources[i].src = inputValue;
+                            }
+                        }
+                        video.load();
+                    });
+                }
+            }
+        }
+
+        function handleEnterKeyPress() {
+            var inputValue = inputElement.value;
+            console.log(`Submitted: ${inputValue}`);
+
+            var currentBackgroundType = getCurrentBackgroundType();
+
+            if (hasFileExtension(inputValue, 'webm') == true || hasFileExtension(inputValue, 'mp4') == true) {
+                handleAnimatedBackround(inputValue, currentBackgroundType);
+            } else if (hasFileExtension(inputValue, 'jpg') == true) {
+                handleStaticBackround(inputValue, currentBackgroundType);
             }
 
             inputElement.value = '';
